13eb5f466cf85ac21a69541eeedc92defc033926
maleacristina <maleacristinaecaterina@gmail.com>
2019-04-14 18:55:06 +0300

    Add autocomplete search using jquery

--- BookStore/BookStore/Controllers/BooksController.cs
+++ BookStore/BookStore/Controllers/BooksController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,30 +11,77 @@ namespace BookStore.Controllers
     [AllowAnonymous]
     public class BooksController : Controller
     {
-        ApplicationDbContext _context;
+        private ApplicationDbContext _context;
 
         public BooksController()
         {
             _context = new ApplicationDbContext();
         }
         // GET: Books
-        public ActionResult Index(string searchBy, string search)
+        //public ActionResult Index(string searchBy, string search)
+        //{
+        //    if (searchBy == "Name")
+        //    {
+        //        return View(_context.Books.Where(x => x.Name.StartsWith(search) || search == null).ToList());
+        //    }
+        //    else
+        //    {
+        //        return View(_context.Books.Where(x => x.Author.StartsWith(search) || search == null).ToList());
+        //    }
+
+
+        //    //if (price >= 10 || price <= 20)
+        //    //    return View(_context.Books.Where(x => x.Price == price).ToList());
+        //    //return View(_context.Books.ToList());
+        //}
+
+        // GET: Books
+        public ActionResult Index()
         {
-            if (searchBy == "Name")
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchTerm)
+        {
+            _context = new ApplicationDbContext();
+            List<Book> books;
+            if (string.IsNullOrEmpty(searchTerm))
             {
-                return View(_context.Books.Where(x => x.Name == search || search == null).ToList());
+                books = _context.Books.ToList();
             }
             else
             {
-                return View(_context.Books.Where(x => x.Author.StartsWith(search) || search == null).ToList());
+                books = _context.Books
+                    .Where(s => s.Name.StartsWith(searchTerm)).ToList();
             }
-            //var book = new Book()
-            //{
-            //    Name = "Baltagul"
-            //};
-            return View(_context.Books.ToList());
+            return View(books);
         }
 
+        public ActionResult getbooks(string term)
+        {
+            return Json(_context.Books.Where(c => c.Name.StartsWith(term)).Select(a => new { label = a.Name, id = a.BookId }), JsonRequestBehavior.AllowGet);
+            //return Json(db.Countries.Where(c => c.Name.StartsWith(term)).Select(a => new { label = a.Name }), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Sort(string sortOrder)
+        {
+            ViewBag.PriceParam = String.IsNullOrEmpty(sortOrder) ? "price_desc" : "";
+            var books = _context.Books.Select(x => x);
+
+            switch (sortOrder)
+            {
+                case "price_desc":
+                    books = books.OrderByDescending(b => b.Price);
+                    break;
+                default:
+                    books = books.OrderBy(b => b.Price);
+                    break;
+            }
+
+            return PartialView(books.ToList());
+        }
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -64,8 +111,25 @@ namespace BookStore.Controllers
         {
             var categories = _context.Categories.ToList();
             //if (User.IsInRole("Admin"))
-               
+        
+
             return PartialView(categories);
         }
+
+  /*      [ChildActionOnly]*/
+        //public ActionResult PriceMenu()
+        //{
+        //    var prices = _context.Books.Select(x => x.Price).ToList();
+
+        //    if (Enumerable.Range(10,50).Contains(prices))
+        //        return PartialView(_context.Books.Where(x => x.Price == 10 || x.Price == 50).ToList());
+        //    else if (price >= 50 || price <= 100)
+        //        return PartialView(_context.Books.Where(x => x.Price == price).ToList());
+        //    else if (price >= 100 || price <= 200)
+        //        return PartialView(_context.Books.Where(x => x.Price == price).ToList());
+        //    return null;
+        //}
     }
+
+    
 }
---
+++ BookStore/BookStore/Scripts/PriceMenu/price.js
@@ -0,0 +1,13 @@
+﻿$("#RegisterViewModel_UserTypeId").change(function () {
+    var selectedValue = $('#RegisterViewModel_UserTypeId :selected').text();
+    var teacherDiv = $("#teacher");
+    var studentDiv = $("#student");
+    if (selectedValue == "Teacher") {
+        teacherDiv.show();
+        studentDiv.hide();
+    } else {
+        teacherDiv.hide();
+        studentDiv.show();
+    }
+
+});
